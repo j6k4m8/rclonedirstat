fe242c958db6ebd82309d1e7e3874d2ea5f9e70b
Jordan Matelsky <j6k4m8@gmail.com>
2024-10-05 09:38:27 -0500

    feat: Add basic draw state up to depth with select

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+/** CLI util to tree from inputs of lines of form `#### <name>`.
+ */
 use clap::{arg, value_parser, Command};
 use fstree::{FSTreeMap, Node};
 use std::cmp::max;
-/** CLI util to tree from inputs of lines of form `#### <name>`.
- */
-// use std::env;
+use std::fmt::Result;
+use std::io;
 use std::{path::PathBuf, process};
+
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    prelude::*,
+    style::Stylize,
+    widgets::Paragraph,
+    widgets::*,
+    DefaultTerminal,
+};
 
 /// Parse the input from the user, either from a file or from stdin.
 ///
@@ -45,15 +55,16 @@ fn cli() -> Command {
             Command::new("sum")
                 .about("Prints the sum of the sizes of the files in the directory tree."),
         )
+        .subcommand(Command::new("run").about("Prints the directory tree."))
         .subcommand(Command::new("tree").about("Prints the directory tree."))
         .args([
             arg!([file] "The file to process").default_value("-"),
             arg!([prefix] "The prefix to search for").default_value(" "),
             arg!(--depth <DEPTH> "The depth of three to unfold")
                 .default_value("0")
-                .value_parser(value_parser!(usize),
-        ),
-        arg!(--human "Prints the sizes in human-readable format")])
+                .value_parser(value_parser!(usize)),
+            arg!(--human "Prints the sizes in human-readable format"),
+        ])
 }
 
 fn pretty_filesize(size_bytes: u64) -> String {
@@ -62,6 +73,27 @@ fn pretty_filesize(size_bytes: u64) -> String {
     let i = max(0, (size.ln() / 1024_f64.ln()).floor() as i32);
     let size = size / 1024_f64.powi(i);
     format!("{:.3} {}", size, units[i as usize])
+}
+
+fn print_tree(node: &Box<Node<u64>>, depth: usize, max_depth: usize) {
+    if depth > max_depth {
+        return;
+    }
+
+    let indent = "  ".repeat(depth);
+    println!(
+        "{}{}: {}",
+        indent,
+        node.get_name(),
+        pretty_filesize(node.value_reduce(0, |a, b| a + b))
+    );
+
+    match node.iter_children() {
+        Ok(child_iter) => child_iter.for_each(|child| {
+            print_tree(&child, depth + 1, max_depth);
+        }),
+        Err(_) => {}
+    }
 }
 
 fn main() {
@@ -103,8 +135,6 @@ fn main() {
             }
         }
         Some(("tree", _)) => {
-            // let trie = create_tree_from_listing(listing);
-
             let mut fs: FSTreeMap<u64> = FSTreeMap::new();
 
             listing
@@ -117,34 +147,98 @@ fn main() {
                     fs.insert_with_parents(path, *size);
                 });
 
-            fn print_tree(node: &Box<Node<u64>>, depth: usize, max_depth: usize) {
-                if depth > max_depth {
-                    return;
-                }
-
-                let indent = "  ".repeat(depth);
-                println!(
-                    "{}{}: {}",
-                    indent,
-                    node.get_name(),
-                    pretty_filesize(node.value_reduce(0, |a, b| a + b))
-                );
-
-                match node.iter_children() {
-                    Ok(child_iter) => child_iter.for_each(|child| {
-                        print_tree(&child, depth + 1, max_depth);
-                    }),
-                    Err(_) => {}
-                }
-            }
-
             for node in fs.iter_children(None).unwrap() {
                 print_tree(node, 0, depth);
             }
+        }
+        Some(("run", _)) => {
+            let mut fs: FSTreeMap<u64> = FSTreeMap::new();
+
+            listing
+                .iter()
+                .filter(|(_, path)| path.starts_with(prefix))
+                .filter(|(size, _)| *size > 0)
+                .for_each(|(size, path)| {
+                    // let path_splits: Vec<String> = path.split("/").map(|s| s.to_string()).collect();
+                    // builder.push(path_splits, *size as u64);
+                    fs.insert_with_parents(path, *size);
+                });
+
+            _ = run_terminal(&fs, prefix, depth, human);
         }
         _ => {
             eprintln!("No subcommand provided");
             process::exit(1);
         }
     }
+}
+
+fn run_terminal(fs: &FSTreeMap<u64>, prefix: &str, depth: usize, human: bool) -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let cleared = terminal.clear()?;
+
+    let mut state = ListState::default().with_selected(Some(0));
+
+    loop {
+        terminal.draw(|frame| {
+            let mut entries: Vec<String> = vec![];
+
+            // Recursively draw tree up to depth:
+            fn add_to_entries(
+                node: &Box<Node<u64>>,
+                depth: usize,
+                max_depth: usize,
+                entries: &mut Vec<String>,
+                human: bool,
+            ) {
+                if depth > max_depth {
+                    return;
+                }
+
+                let indent = "  ".repeat(depth);
+                let size = node.value_reduce(0, |a, b| a + b);
+                let size = if human {
+                    pretty_filesize(size)
+                } else {
+                    size.to_string()
+                };
+                entries.push(format!("{}{}: {}", indent, node.get_name(), size));
+
+                match node.iter_children() {
+                    Ok(child_iter) => child_iter.for_each(|child| {
+                        add_to_entries(&child, depth + 1, max_depth, entries, human);
+                    }),
+                    Err(_) => {}
+                }
+            }
+
+            add_to_entries(&fs.root, 0, depth, &mut entries, human);
+
+            let list = List::new(entries)
+                .block(Block::bordered().title(prefix))
+                .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
+                .highlight_symbol(">>")
+                .repeat_highlight_symbol(true);
+
+            frame.render_stateful_widget(list, frame.area(), &mut state);
+        })?;
+
+        if let event::Event::Key(key) = event::read().unwrap() {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            } else {
+                match key.code {
+                    KeyCode::Up => {
+                        state.select_previous();
+                    }
+                    KeyCode::Down => {
+                        state.select_next();
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    ratatui::restore();
 }
